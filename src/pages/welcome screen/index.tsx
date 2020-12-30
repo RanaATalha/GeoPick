cb0cdedab244b3999c521587dee5bb54c42af901
mk210 <55575735+mk210@users.noreply.github.com>
2020-12-30 11:22:07 +0400

    Refactored Welcome page

--- src/pages/welcome screen/index.tsx
+++ src/pages/welcome screen/index.tsx
@@ -1,31 +1,64 @@
 import * as React from 'react';
 import './styles.scss';
-import WhiteLogo from "./WhiteLogo.svg";
-import googleButton from "./googleButton.svg";
-interface WelcomeProps {
-    
-}
+import WhiteLogo from './WhiteLogo.svg';
+import googleButton from './googleButton.svg';
+import background from './welcome-pg.png';
+import { Grid, Typography } from '@material-ui/core';
+import Checkbox from '../../components/Inputs/Checkbox';
+import TextField from '../../components/Inputs/TextField';
+import PasswordField from '../../components/Inputs/PasswordField';
+import Card from '../../components/Layouts/Card';
+import { RegularBtn } from '../../components/Buttons/RegularBtn';
+import { Widgets } from '@material-ui/icons';
+interface WelcomeProps {}
 
 const WelcomeScreen: React.FunctionComponent<WelcomeProps> = (props) => {
     return (
-    <html>
-        <body>
-        
-            <div className="welcomepg">
-                <img src={WhiteLogo}/>
-            
-                <p className="caption"><span>A tool for</span><br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;shutterbugs!</p>
-               <div className= "">
-                    <button className="g-btn"><img src={googleButton} width="22px"/><p className="text">Sign in</p></button>
-                
-               </div>
-                   <p className="or">-----------------or-----------------</p>
-                   <button className="sign-btn">Sign up now!</button><br></br>
-                   <button className="sign-btn1" >Sign in</button>
-            
-            </div>
-        </body>
-    </html>);
+        <html>
+            <body
+                style={{
+                    backgroundImage: `url(${background})`,
+                }}
+            >
+                {/* <img src ={background} width='auto'> */}
+                <div className="welcomepg">
+                    <Grid container spacing={4} direction="column" alignItems="center" justify="center">
+                        <Grid item>
+                            <img src={WhiteLogo} alt="GeoPicK Logo" />
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="h5" align="center">
+                                <span>A tool for</span>
+                                <br></br>shutterbugs!
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <button className="g-btn">
+                                <img src={googleButton} width="22px" alt="Google" />
+                                <p className="text">Sign in</p>
+                            </button>
+                        </Grid>
+                        <Grid item>
+                            {/* <p className="or">or</p> */}
+                            <hr />
+                        </Grid>
+                        <Grid item>
+                            <button className="sign-btn">Sign up now!</button>
+                            {/* <br></br> */}
+                            <button className="sign-btn1">Sign in</button>
+                        </Grid>
+                    </Grid>
+
+                    {/* <p className="caption"> */}
+
+                    {/* </p> */}
+                    {/* <div className=""></div> */}
+
+                    {/* <br></br> */}
+                </div>
+            </body>
+        </html>
+    );
 };
 
 export default WelcomeScreen;
