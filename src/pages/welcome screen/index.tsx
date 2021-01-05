b6bdf046f8c07514ccb86d6609f285ec0a05b255
mk210 <55575735+mk210@users.noreply.github.com>
2021-01-05 10:25:01 +0400

    Updated welcome screen and regButton
    Updated the buttons in welcome screen and added a missing color in the reg Button component.

--- src/pages/welcome screen/index.tsx
+++ src/pages/welcome screen/index.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import './styles.scss';
 import SignUpScreen from '../sign-up screen/index';
-import { BrowserRouter as Router, Route, Switch, Link, BrowserRouter, } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link, BrowserRouter } from 'react-router-dom';
 import WhiteLogo from './WhiteLogo.svg';
 import googleButton from './googleButton.svg';
-import background from './welcome-pg.png';
+// import background from './welcome-pg.png';
 import { createStyles, Grid, makeStyles, Theme, Typography } from '@material-ui/core';
+import Divider from '@material-ui/core/Divider';
 import Checkbox from '../../components/Inputs/Checkbox';
 import TextField from '../../components/Inputs/TextField';
 import PasswordField from '../../components/Inputs/PasswordField';
@@ -31,11 +32,7 @@ const WelcomeScreen: React.FunctionComponent<WelcomeProps> = (props) => {
 
     return (
         <html>
-            <body
-                style={{
-                    backgroundImage: `url(${background})`,
-                }}
-            >
+            <body>
                 {/* <img src ={background} width='auto'> */}
                 <div className="welcomepg">
                     <Grid container spacing={10} direction="column" alignItems="center" justify="center">
@@ -49,30 +46,39 @@ const WelcomeScreen: React.FunctionComponent<WelcomeProps> = (props) => {
                             </Typography>
                         </Grid>
                         <Grid container spacing={3} direction="column" alignItems="center" justify="center">
-                            <Grid item xs={2}
-                                            alignContent="center"
-                                            alignItems="center"
-                                            style={{ verticalAlign: 'true' }}>
-                            </Grid>
-
-                                <Grid item xs={12} alignItems="center" justify="center" style={{ textAlign: 'center' }}>
-                                    <RegularBtn colorType="dark" style={{border:"orange", width: '150px', borderRadius: '15px', height:'50px' }}>
+                            <Grid
+                                item
+                                xs={2}
+                                alignContent="center"
+                                alignItems="center"
+                                style={{ verticalAlign: 'true' }}
+                            ></Grid>
+                            <Grid item xs={12} alignItems="center" justify="center" style={{ textAlign: 'center' }}>
+                                <RegularBtn
+                                    colorType="dark"
+                                    style={{ border: 'orange', width: '150px', borderRadius: '15px', height: '50px' }}
+                                >
                                     <img src={googleButton} width="22px" alt="Google" />
-                                    <h1 style={{color:"white" , fontSize:"15px"}}>Sign in </h1> 
-                                    </RegularBtn>
-                                </Grid>
-                    __________________________________
-                                <Grid item xs={12} alignItems="center" justify="center" style={{ textAlign: 'center' }}>
-                                    <RegularBtn colorType="auto"  style={{ background:"Orange" ,color:"white", width: '150px',height:'50px' ,borderRadius: '15px' }}>
-                                    <h1 style={{color:"white" , fontSize:"15px"}}>Sign Up Now! </h1> 
-                                    </RegularBtn>
-                                </Grid>
-
-                                <Grid item xs={12} alignItems="center" justify="center" style={{ textAlign: 'center' }}>
-                                    <RegularBtn colorType="dark" style={{ width: '150px',height:'50px' ,borderRadius: '15px' }}>
-                                    <h1 style={{color:"white" , fontSize:"15px"}}>Sign in </h1> 
-                                    </RegularBtn>
-                                </Grid>
+                                    <h1 style={{ color: 'white', fontSize: '15px' }}>Sign in </h1>
+                                </RegularBtn>
+                            </Grid>
+                            <Divider variant="middle" orientation="horizontal" />
+                            <Grid item xs={12} alignItems="center" justify="center" style={{ textAlign: 'center' }}>
+                                <RegularBtn
+                                    colorType="orange"
+                                    style={{ color: 'white', width: '150px', height: '50px', borderRadius: '15px' }}
+                                >
+                                    <h1 style={{ color: 'white', fontSize: '15px' }}>Sign Up Now! </h1>
+                                </RegularBtn>
+                            </Grid>
+                            <Grid item xs={12} alignItems="center" justify="center" style={{ textAlign: 'center' }}>
+                                <RegularBtn
+                                    colorType="dark"
+                                    style={{ width: '150px', height: '50px', borderRadius: '15px' }}
+                                >
+                                    <h1 style={{ color: 'white', fontSize: '15px' }}>Sign in </h1>
+                                </RegularBtn>
+                            </Grid>
                         </Grid>
                     </Grid>
                 </div>
