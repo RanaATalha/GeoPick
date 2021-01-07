d06c6fab448d341e2e5f2b367f2daec00acfa24b
GliderCodes <thegoglider@gmail.com>
2021-01-07 19:27:46 +0400

    code splitting sign up

--- src/pages/sign-up screen/index.tsx
+++ src/pages/sign-up screen/index.tsx
@@ -11,71 +11,70 @@ export interface SignUpProps {}
 export default class SignUpScreen extends React.Component<SignUpProps> {
     public render(): JSX.Element {
         return (
-            <html>
-                <body>
-                    <div className="PageSignUp">
-                        <Card>
-                            <Grid container spacing={4} direction="row" alignItems="center" justify="center">
-                                <Grid item>
-                                    <Typography variant="h3" style={{ color: 'white', fontWeight: 'unset' }}>
-                                        Setup your account!
-                                    </Typography>
-                                </Grid>
-                                <form>
-                                    <Grid
-                                        item
-                                        container
-                                        spacing={3}
-                                        direction="row"
-                                        alignItems="center"
-                                        justify="center"
-                                    >
-                                        <Grid item style={{ width: '100%' }}>
-                                            <TextField label="Email" />
-                                        </Grid>
-                                        <Grid item style={{ width: '100%' }}>
-                                            <PasswordField />
-                                        </Grid>
-                                        <Grid item style={{ width: '100%' }}>
-                                            <PasswordField label="Confirm Password" />
-                                        </Grid>
-                                    </Grid>
-                                    <Grid item container spacing={3}>
-                                        <Grid
-                                            item
-                                            xs={2}
-                                            alignContent="center"
-                                            alignItems="center"
-                                            style={{ verticalAlign: 'true' }}
-                                        >
-                                            <Checkbox required />
-                                        </Grid>
-                                        <Grid
-                                            item
-                                            xs={10}
-                                            alignContent="center"
-                                            alignItems="center"
-                                            style={{ paddingTop: '20px', verticalAlign: 'true' }}
-                                        >
-                                            <Typography align="left" style={{ fontSize: '12px', color: '1B1B1E' }}>
-                                                By signing up I conform that I have read and accepted the terms and
-                                                conditions of using the application.
-                                            </Typography>
-                                        </Grid>
-                                    </Grid>
-
-                                    <br></br>
-                                </form>
-                                <Grid item xs={12} alignItems="center" justify="center" style={{ textAlign: 'center' }}>
-                                    <RegularBtn colorType="white" style={{ width: '50%', borderRadius: '15px' }}>
-                                        Sign Up!
-                                    </RegularBtn>
-                                </Grid>
-                            </Grid>
-                        </Card>
-                    </div>
-                </body>
-            </html>
+            <Card>
+                <Grid container spacing={4} direction="row" alignItems="center" justify="center">
+                    <SignUpHeader title="Setup your account" />
+                    <SignUpForm />
+                </Grid>
+            </Card>
         );
     }
 }
+
+const SignUpHeader = (props: { title: string }) => {
+    return (
+        <Grid item>
+            <Typography variant="h3" style={{ color: 'white', fontWeight: 'unset' }}>
+                {props.title}
+            </Typography>
+        </Grid>
+    );
+};
+
+const SignUpFields = () => {
+    return (
+        <Grid item container spacing={3} direction="row" alignItems="center" justify="center">
+            <Grid item style={{ width: '100%' }}>
+                <TextField label="Email" />
+            </Grid>
+            <Grid item style={{ width: '100%' }}>
+                <PasswordField />
+            </Grid>
+            <Grid item style={{ width: '100%' }}>
+                <PasswordField label="Confirm Password" />
+            </Grid>
+        </Grid>
+    );
+};
+
+const SignUpForm = () => {
+    return (
+        <>
+            <form>
+                <SignUpFields />
+                <Grid item container spacing={3}>
+                    <Grid item xs={2} alignContent="center" alignItems="center" style={{ verticalAlign: 'true' }}>
+                        <Checkbox required />
+                    </Grid>
+                    <Grid
+                        item
+                        xs={10}
+                        alignContent="center"
+                        alignItems="center"
+                        style={{ paddingTop: '20px', verticalAlign: 'true' }}
+                    >
+                        <Typography align="left" style={{ fontSize: '12px', color: '1B1B1E' }}>
+                            By signing up I conform that I have read and accepted the terms and conditions of using the
+                            application.
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </form>
+            <Grid item xs={12} alignItems="center" justify="center" style={{ textAlign: 'center' }}>
+                <RegularBtn colorType="white" style={{ width: '50%', borderRadius: '15px' }}>
+                    Sign Up!
+                </RegularBtn>
+            </Grid>
+        </>
+    );
+};
