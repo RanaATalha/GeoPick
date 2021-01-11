215076e1d885393680591f8cc018cb56b5a1dc3f
mk210 <55575735+mk210@users.noreply.github.com>
2021-01-11 12:52:39 +0400

    Added Single post - draft

---
+++ src/components/singlePost/post.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Component } from 'react';
+
+export interface PostProps {}
+
+export interface PostState {}
+
+class Post extends React.Component<PostProps, PostState> {
+    // state = { :  }
+    render() {
+        return <></>;
+    }
+}
+
+export default Post;
