9e8ed391067e5b349bebfb1d4bece6d21c59d2bc
adt6 <aditya.thakur5753235@gmail.com>
2021-01-13 11:18:49 -0800

    storage class implemented

---
+++ src/firebase/storage.tsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import {storage} from "./firebase";
+
+const ImageInput = {imgUrl: ''}
+const [imageAsFile, setImageAsFile] = useState('')
+const [imageAsUrl, setImageAsUrl] = useState(ImageInput)
+
+const handleImageAsFile = (e) => {
+    setImageAsFile(e.target.files[0])
+}
+
+const handleFireBaseUpload = (e) => {
+    e.preventDefault()
+}
