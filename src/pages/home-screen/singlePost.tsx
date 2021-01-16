9d105a701f6c30d00d877fa451572c085eb92130
mk210 <55575735+mk210@users.noreply.github.com>
2021-01-16 13:43:15 +0400

    Updated Single post interface
    added GeoPick button(beta), comment box, like and share buttons..

--- src/pages/home-screen/singlePost.tsx
+++ src/pages/home-screen/singlePost.tsx
@@ -1,7 +1,12 @@
-import { Avatar, Grid, Card, Typography } from '@material-ui/core';
+import { Avatar, Grid, Card, Typography, IconButton, TextField, Button, Container } from '@material-ui/core';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
 import * as React from 'react';
 import { Component } from 'react';
 import './singlePostStyles.scss';
+
+import InputBase from '@material-ui/core/InputBase';
+import PublishRoundedIcon from '@material-ui/icons/PublishRounded';
 export interface SinglePostProps {
     username?: string;
     postPic?: string;
@@ -29,7 +34,7 @@ class SinglePost extends Component<SinglePostProps, SinglePostState> {
                             </Typography>
                         </Card>
                     </Grid>
-                    <Grid item justify="flex-end" style={{ marginLeft: '20%' }}>
+                    <Grid item justify="flex-end" style={{ marginLeft: '18%' }}>
                         <Card
                             style={{
                                 color: '#F56920',
@@ -45,19 +50,53 @@ class SinglePost extends Component<SinglePostProps, SinglePostState> {
                     </Grid>
                 </Grid>
                 <br></br>
-                <Grid container direction="row" spacing={1} justify={'center'}>
-                    <div className="postImage" style={{ justifyItems: 'center' }}>
+                {/* <Grid container direction="row" spacing={2} justify={'center'}>
+                    <Grid item> */}
+                <Container fixed>
+                    <div className="postImage" style={{ justifyItems: 'normal' }}>
                         <img
                             src={this.props.postPic}
                             alt="not loading..."
-                            width="40%"
+                            width="50%"
                             height="90%"
                             className="postImage"
                             style={{ borderRadius: '20px 20px 0px 0px' }}
                         ></img>
-                        {/* add GuessTheLocation button here.. */}
+                        <Button style={{ transform: 'translate(-130%, -50%)' }}>GeoPick</Button>
                     </div>
+                </Container>
+
+                {/* </Grid>
+                </Grid> */}
+                <Grid container direction="column" spacing={2} justify={'center'}>
+                    <Grid item justify="flex-start" direction="column" style={{ marginLeft: '-15%' }}>
+                        <div style={{ alignContent: 'flex-start', justifyContent: 'left' }}>
+                            <IconButton aria-label="add to favorites" style={{ color: '#FAFAFA' }}>
+                                <FavoriteIcon />
+                            </IconButton>
+                            <IconButton aria-label="share" style={{ color: '#FAFAFA' }}>
+                                <ShareIcon />
+                            </IconButton>
+                            {/* <Grid item> */}
+                            <TextField
+                                label="Start typing..."
+                                variant="outlined"
+                                InputProps={{
+                                    endAdornment: (
+                                        <IconButton
+                                            aria-label="upload"
+                                            style={{ color: '#FAFAFA', alignContent: 'end' }}
+                                        >
+                                            <PublishRoundedIcon />
+                                        </IconButton>
+                                    ),
+                                }}
+                            />
+                            {/* </Grid> */}
+                        </div>
+                    </Grid>
                 </Grid>
+
                 <div style={{ padding: '25px' }}></div>
             </>
         );
@@ -65,3 +104,5 @@ class SinglePost extends Component<SinglePostProps, SinglePostState> {
 }
 
 export default SinglePost;
+
+// {/* <IconButton type="submit" aria-label="submit" style={{ color: '#FAFAFA' }}></IconButton> */}
