3abe1ca34ed20dc2ef0f853b2f2e162df53c32d1
JeetSolanki999 <jeetsolanki3012@gmail.com>
2021-01-18 11:56:16 +0300

    added forms to signin

--- src/pages/sign-in screen/index.tsx
+++ src/pages/sign-in screen/index.tsx
@@ -8,6 +8,7 @@ import { RegularBtn } from '../../components/Buttons/RegularBtn';
 import { Link } from 'react-router-dom';
 import './styles.scss';
 import { auth } from '../../firebase';
+import { useForm } from 'react-hook-form';
 export interface SignInProps {}
 
 export default class SignInScreen extends React.Component<SignInProps> {
@@ -22,41 +23,60 @@ export default class SignInScreen extends React.Component<SignInProps> {
                 console.log("Error "+ err.toString);
             })
         };*/
-        
+
+        const SignInFunction = ({ register, errors }: { register: any; errors: any }) => {
+
         return (
             <div>
                 <div className="bg">
                     <Card background="white" title="Sign In" split={1}>
-                        <Grid container spacing={4} direction="row">
-                            <Grid item>
-                                <Typography align="left" variant="h3">
-                                    <h5>You know what to do... </h5>
-                                </Typography>
+                        <form onSubmit={handleSubmit(SignInFunction)}>
+                            <Grid container spacing={4} direction="row">
+                                <Grid item>
+                                    <Typography align="left" variant="h3">
+                                        <h5>You know what to do... </h5>
+                                    </Typography>
+                                </Grid>
+
+                                <Grid container spacing={2}>
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            name="email"
+                                            id="Email"
+                                            label="Email"
+                                            type="email"
+                                            inputRef={register({
+                                                required: true,
+                                                pattern: {
+                                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                                    message: 'invalid email address',
+                                                },
+                                            })}
+                                            error={errors.email ? true : false}
+                                        />
+                                    </Grid>
+
+                                    <Grid item xs={12}>
+                                        <PasswordField id="Password"> </PasswordField>
+                                    </Grid>
+
+                                    <Grid item xs={12}>
+                                        <RegularBtn colorType="orange" style={{ width: '50%', borderRadius: '18px' }}>
+                                            Sign In
+                                        </RegularBtn>
+                                    </Grid>
+
+                                    <Grid item xs={12}>
+                                        <Link to="/ReSet-password"> Forgot Password ?</Link>
+                                    </Grid>
+                                </Grid>
                             </Grid>
-
-                            <Grid container spacing={2}>
-                                <Grid item xs={12}>
-                                    <TextField id="Email" label="Email"> </TextField>
-                                </Grid>
-
-                                <Grid item xs={12}>
-                                    <PasswordField id="Password"> </PasswordField>
-                                </Grid>
-
-                                <Grid item xs={12}>
-                                    <RegularBtn colorType="orange" style={{ width: '50%', borderRadius: '18px' }}>
-                                        Sign In
-                                    </RegularBtn>
-                                </Grid>
-
-                                <Grid item xs={12}>
-                                    <Link to="/ReSet-password"> Forgot Password ?</Link>
-                                </Grid>
-                            </Grid>
-                        </Grid>
+                        </form>
                     </Card>
+                    <br />
                 </div>
             </div>
         );
+            };
     }
 }
