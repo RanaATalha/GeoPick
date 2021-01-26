e1722ab77d4d42ca4287cf9f7d7df0580dac8fa2
JeetSolanki999 <jeetsolanki3012@gmail.com>
2021-01-26 10:28:53 +0300

    Comments getting uploaded to the db

--- src/components/Display/sharePost.tsx
+++ src/components/Display/sharePost.tsx
@@ -29,82 +29,115 @@ import {
 } from 'react-share';
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      transform: 'translateZ(0px)',
-      flexGrow: 1,
-      backgroundColor: 'transparent'
-    },
-    speedDial: {
-      '&.MuiFab-primary' : {
-        backgroundColor: 'transparent',
-      },
-      width: '40px',
-      height: '40px',
-    },
-    options: {
-      width: '0px',
-      height: '0px',
-      padding: '10px',
-      backgroundColor: 'transparent',
-    },
-  }),
+    createStyles({
+        root: {
+            transform: 'translateZ(0px)',
+            flexGrow: 1,
+            backgroundColor: 'transparent',
+        },
+        speedDial: {
+            '&.MuiFab-primary': {
+                backgroundColor: 'transparent',
+            },
+            width: '40px',
+            height: '40px',
+        },
+        options: {
+            width: '0px',
+            height: '0px',
+            padding: '10px',
+            backgroundColor: 'transparent',
+        },
+    }),
 );
 
+export default function SharePost({ sharedURL }: { sharedURL: string }) {
+    const classes = useStyles();
+    const [direction, setDirection] = React.useState<SpeedDialProps['direction']>('right');
+    const [open, setOpen] = React.useState(false);
+    const [hidden, setHidden] = React.useState(false);
 
+    const actions = [
+        {
+            icon: (
+                <FacebookShareButton url={sharedURL}>
+                    <FacebookIcon size={32} round />
+                </FacebookShareButton>
+            ),
+            name: 'Facebook',
+        },
+        {
+            icon: (
+                <TwitterShareButton url={sharedURL}>
+                    <TwitterIcon size={32} round />
+                </TwitterShareButton>
+            ),
+            name: 'Twitter',
+        },
+        {
+            icon: (
+                <TelegramShareButton url={sharedURL}>
+                    <TelegramIcon size={32} round />
+                </TelegramShareButton>
+            ),
+            name: 'Telegram',
+        },
+        {
+            icon: (
+                <WhatsappShareButton url={sharedURL}>
+                    <WhatsappIcon size={32} round />
+                </WhatsappShareButton>
+            ),
+            name: 'WhatsApp',
+        },
+        {
+            icon: (
+                <EmailShareButton url={sharedURL}>
+                    <EmailIcon size={32} round />
+                </EmailShareButton>
+            ),
+            name: 'Email',
+        },
+    ];
 
-export default function SharePost({sharedURL}: {sharedURL: string;}) {
-  const classes = useStyles();
-  const [direction, setDirection] = React.useState<SpeedDialProps['direction']>('right');
-  const [open, setOpen] = React.useState(false);
-  const [hidden, setHidden] = React.useState(false);
+    const handleDirectionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setDirection((event.target as HTMLInputElement).value as SpeedDialProps['direction']);
+    };
 
-  const actions = [
-    { icon: <FacebookShareButton url={sharedURL}><FacebookIcon size={32} round /></FacebookShareButton>, name: 'Facebook' },
-    { icon: <TwitterShareButton url={sharedURL}><TwitterIcon size={32} round /></TwitterShareButton>, name: 'Twitter' },
-    { icon: <TelegramShareButton url={sharedURL}><TelegramIcon size={32} round /></TelegramShareButton>, name: 'Telegram' },  
-    { icon: <WhatsappShareButton url={sharedURL}><WhatsappIcon size={32} round /></WhatsappShareButton>, name: 'WhatsApp' },
-    { icon: <EmailShareButton url={sharedURL}><EmailIcon size={32} round /></EmailShareButton>, name: 'Email' },
-  ];
+    const handleHiddenChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setHidden(event.target.checked);
+    };
 
-  const handleDirectionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setDirection((event.target as HTMLInputElement).value as SpeedDialProps['direction']);
-  };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-  const handleHiddenChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setHidden(event.target.checked);
-  };
+    const handleOpen = () => {
+        setOpen(true);
+    };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  return (
-    //   <div className={classes.exampleWrapper}>
+    return (
+        //   <div className={classes.exampleWrapper}>
         <SpeedDial
-          ariaLabel="SpeedDial example"
-          className={classes.speedDial}
-          hidden={hidden}
-          icon={<ShareIcon />}
-          onClose={handleClose}
-          onOpen={handleOpen}
-          open={open}
-          direction={direction}
+            ariaLabel="SpeedDial example"
+            className={classes.speedDial}
+            hidden={hidden}
+            icon={<ShareIcon />}
+            onClose={handleClose}
+            onOpen={handleOpen}
+            open={open}
+            direction={direction}
         >
-          {actions.map((action) => (
-            <SpeedDialAction
-              key={action.name}
-              className={classes.options}
-              icon={action.icon}
-              tooltipTitle={action.name}
-              onClick={handleClose}
-            />
-          ))}
+            {actions.map((action) => (
+                <SpeedDialAction
+                    key={action.name}
+                    className={classes.options}
+                    icon={action.icon}
+                    tooltipTitle={action.name}
+                    onClick={handleClose}
+                />
+            ))}
         </SpeedDial>
-    //   </div>
-  );
+        //   </div>
+    );
 }
--- src/pages/post-view/index.tsx
+++ src/pages/post-view/index.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
+import InputBase from '@material-ui/core/InputBase';
+import { render } from '@testing-library/react';
+import { IconButton } from '@material-ui/core';
+import PublishRoundedIcon from '@material-ui/icons/PublishRounded';
+import fb from 'firebase/app';
 
-interface PostViewProps {}
+export interface PostViewProps {
+    newComment: string;
+}
 
 // const useStyles = makeStyles((theme: Theme) =>
 //     createStyles({
@@ -14,26 +21,71 @@ interface PostViewProps {}
 //         },
 //     }),
 // );
-const PostView: React.FunctionComponent<PostViewProps> = (props) => {
-    // const classes = useStyles();
-    /* var config={
+// const PostView: React.FunctionComponent<PostViewProps> = (props) =>
+// const classes = useStyles();
+/* var config={
    apiKey:"",
    authDomain:"",
    databaseURL:"",
    project:"",
    storeageBucket:"",
    messageingSenderid:""
-};*/
+;*/
 
-    return (
-        <html>
-            <body>
-                {/* <img src ={background} width='auto'> */}
-                <div className="welcomepg">
-                </div>
-            </body>
-        </html>
-    );
-};
+export class PostViewScreen extends React.Component<{}, { newComment: string }> {
+    render() {
+        const handleChange = (event: any) => {
+            console.log(event.target.value);
+            this.setState({ newComment: event.target.value as string });
+        };
 
-export default PostView;
+        const handleClick = (event: any) => {
+            const FieldValue = fb.firestore.FieldValue;
+            const comment = this.state.newComment;
+            fb.firestore()
+                .collection('Posts')
+                .doc('tBKONbLunJRyvL1Td0SB')
+                .update({
+                    comments: FieldValue.arrayUnion(comment),
+                    comments_count: fb.firestore.FieldValue.increment(1),
+                });
+            console.log('state', comment);
+        };
+
+        return (
+            <html>
+                <body>
+                    {/* <img src ={background} width='auto'> */}
+                    <div className="welcomepg">
+                        <InputBase
+                            onChange={handleChange}
+                            placeholder="Start typing..."
+                            style={{
+                                width: '32.5%',
+                                marginRight: '-12%',
+                                textDecorationColor: '#FAFAFA',
+                                border: '1px solid #FAFAFA',
+                                borderRadius: '10px',
+                                height: '50px',
+                                padding: '10px',
+                                color: 'black',
+                            }}
+                            endAdornment={
+                                <IconButton
+                                    onClick={handleClick}
+                                    aria-label="upload"
+                                    type="submit"
+                                    style={{ color: '#FAFAFA', alignContent: 'end' }}
+                                >
+                                    <PublishRoundedIcon />
+                                </IconButton>
+                            }
+                        />
+                    </div>
+                </body>
+            </html>
+        );
+    }
+}
+
+export default PostViewScreen;
