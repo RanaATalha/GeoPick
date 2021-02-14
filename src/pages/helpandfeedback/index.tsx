43b18ff3e53b969ee9f2f75e12369040250cbd40
JeetSolanki999 <jeetsolanki3012@gmail.com>
2021-02-14 23:06:10 +0300

    feedback update

--- src/pages/helpandfeedback/index.tsx
+++ src/pages/helpandfeedback/index.tsx
@@ -4,24 +4,36 @@ import './styles.scss';
 import Card from '../../components/Layouts/Card';
 import { RegularBtn } from '../../components/Buttons/RegularBtn';
 import TextField from '../../components/Inputs/TextField';
+import ArrowBackRoundedIcon from '@material-ui/icons/ArrowBackRounded';
 
 export interface HelpandFeedbackProps {}
 
 export default function HelpandFeedback() {
     return (
         <div className="background">
+            <div className="button" style={{ float: 'left' }}>
+                <ArrowBackRoundedIcon />
+            </div>
+
             <div className="image">
                 <img src={WhiteLogo} alt="GeoPicK Logo" />
             </div>
 
-            <Card background="black" title="Help & FeedBack" split={1}>
+            <Card background="black" title="Help & FeedBack" split={2}>
                 <p>Hi there,</p>
                 <p> This is our first phone based web application. We would love to hear your Feedback.</p>
                 <p> You can either mail the developer team or send any message via the feedback form.</p>
                 <p>Thanks,</p>
                 <p>The GeoPick Dev Team</p>
 
-                <TextField id="standard-multiline-flexible" label="Feedback" multiline rowsMax={4} />
+                <TextField
+                    id="standard-multiline-flexible"
+                    label="Feedback"
+                    variant="outlined"
+                    placeholder="Write something..."
+                    multiline
+                    rowsMax={5}
+                />
             </Card>
 
             <RegularBtn type="submit" colorType="orange" style={{ width: '40%', borderRadius: '15px' }}>
