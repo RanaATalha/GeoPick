d79a56197ab42706b4bd6c4e67a5f74d7f7e2e12
mk210 <55575735+mk210@users.noreply.github.com>
2021-02-14 11:53:49 +0400

    added Nav and modified settings menu

--- src/pages/settings-screen/SettingsMenu.tsx
+++ src/pages/settings-screen/SettingsMenu.tsx
@@ -7,7 +7,7 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Divider from '@material-ui/core/Divider';
-import {Grid} from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import WhiteLogo from '../welcome screen/WhiteLogo.svg';
 import './styles.scss';
 import { checkUserLoggedIn } from '../../firebase/auth';
@@ -17,103 +17,100 @@ import NotificationImportantRoundedIcon from '@material-ui/icons/NotificationImp
 import AccessibilityNewRoundedIcon from '@material-ui/icons/AccessibilityNewRounded';
 import HelpRoundedIcon from '@material-ui/icons/HelpRounded';
 import GavelRoundedIcon from '@material-ui/icons/GavelRounded';
-export interface SettingsMenuProps {}    
+export interface SettingsMenuProps {}
 export interface SettingsMenuState {
     user: any;
 }
 
-
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      width: '100%',
-      maxWidth: 360,
-      backgroundColor: theme.palette.background.paper,
-    },
-  }),
+    createStyles({
+        root: {
+            width: '100%',
+            maxWidth: 360,
+            // backgroundColor: theme.palette.background.paper,
+            backgroundColor: '#fafafa',
+        },
+    }),
 );
 
 const SettingsMenu = () => {
     const classes = useStyles();
-    return(
-        <div className="bggg" >
-        <List component="nav" className={classes.root} aria-label="mailbox folders">
-        <ListItem button>
-        <ListItemAvatar>
-          <Avatar>
-            <AccountCircleRoundedIcon style={{ color: 'black' }}  />
-          </Avatar>
-        </ListItemAvatar>
-            <ListItemText primary="Account Settings" />
-        </ListItem>
-        <Divider />
-        <ListItem button divider>
-        <ListItemAvatar>
-          <Avatar>
-            <NotificationImportantRoundedIcon style={{ color: 'black' }} />
-          </Avatar>
-        </ListItemAvatar>
-            <ListItemText primary="Notification Settings" />
-        </ListItem>
-        <ListItem button>
-        <ListItemAvatar>
-          <Avatar>
-            <AccessibilityNewRoundedIcon style={{ color: 'black' }} />
-          </Avatar>
-        </ListItemAvatar>
-            <ListItemText primary="Accessibility Settings" />
-        </ListItem>
-        <Divider light />
-        <ListItem button>
-        <ListItemAvatar>
-          <Avatar>
-            <HelpRoundedIcon style={{ color: 'black' }} />
-          </Avatar>
-        </ListItemAvatar>
-            <ListItemText primary="Help and Feedback" />
-        </ListItem>
-        <ListItem button>
-        <ListItemAvatar>
-          <Avatar>
-            <GavelRoundedIcon style={{ color: 'black' }} />
-          </Avatar>
-        </ListItemAvatar>
-            <ListItemText primary="Terms and Conditions" />
-        </ListItem>
-        </List>
+    return (
+        <div style={{ width: '100%', height: '100%' }}>
+            <List component="nav" className={classes.root} aria-label="mailbox folders">
+                <ListItem button>
+                    <ListItemAvatar>
+                        <Avatar style={{ background: '#fafafa' }}>
+                            <AccountCircleRoundedIcon style={{ color: 'black' }} />
+                        </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="Account Settings" />
+                </ListItem>
+                <Divider />
+                <ListItem button divider>
+                    <ListItemAvatar>
+                        <Avatar style={{ background: '#fafafa' }}>
+                            <NotificationImportantRoundedIcon style={{ color: 'black' }} />
+                        </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="Notification Settings" />
+                </ListItem>
+                <ListItem button divider>
+                    <ListItemAvatar>
+                        <Avatar style={{ background: '#fafafa' }}>
+                            <AccessibilityNewRoundedIcon style={{ color: 'black' }} />
+                        </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="Accessibility Settings" />
+                </ListItem>
+                <ListItem button divider>
+                    <ListItemAvatar>
+                        <Avatar style={{ background: '#fafafa' }}>
+                            <HelpRoundedIcon style={{ color: 'black' }} />
+                        </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="Help and Feedback" />
+                </ListItem>
+                <ListItem button divider>
+                    <ListItemAvatar>
+                        <Avatar style={{ background: '#fafafa' }}>
+                            <GavelRoundedIcon style={{ color: 'black' }} />
+                        </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="Terms and Conditions" />
+                </ListItem>
+            </List>
         </div>
-    )
-}
-    
+    );
+};
 
-export default class SettingsMenuScreen extends Component<SettingsMenuProps,SettingsMenuState> {
+export default class SettingsMenuScreen extends Component<SettingsMenuProps, SettingsMenuState> {
     constructor(SettingsMenuProps: any) {
         super(SettingsMenuProps);
         this.state = {
             user: checkUserLoggedIn(),
         };
     }
-        render() {
-            
-            return (
-        <div style={{ background: '#1b1b1b' }} className="bgg">
-            <Toolbar>
+    render() {
+        return (
+            <div style={{ background: '#1b1b1b' }} className="bgg">
+                <Toolbar>
                     <img src={WhiteLogo} alt="GeoPicK" className="WhiteLogo" />
                     <IconButton edge="end">
                         <Avatar alt={this.state.user.User_name} src={this.state.user.Avatar} />
                     </IconButton>
                 </Toolbar>
-             <div style={{color:'white'}}>
-            <Card  background="white" title="Settings"  split={1}>
-                <div style={{color:'black'}}>
-                <Grid container spacing={4} direction="row" alignItems="center" justify="center">
-                    <SettingsMenu />
-                </Grid>
+                <div style={{ color: '#fafafa' }}>
+                    <Card background="#fafafa" title="Settings" split={1}>
+                        <div style={{ color: 'black' }}>
+                            <Grid container spacing={4} direction="row" alignItems="center" justify="center">
+                                <SettingsMenu />
+                            </Grid>
+                        </div>
+                    </Card>
                 </div>
-            </Card>
+                <br />
             </div>
-            <br />
-        </div>
-    );
-            }
- }
+        );
+    }
+}
