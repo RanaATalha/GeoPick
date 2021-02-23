c91d1baf28d2539a845a195d6fe2bc976903d1c4
adt6 <aditya.thakur5753235@gmail.com>
2021-02-22 23:01:52 -0800

    Accessibllity settings
    Please align this also
    
    Co-Authored-By: Mohit Katta <55575735+mk210@users.noreply.github.com>

--- src/pages/accessSetting/index.tsx
+++ src/pages/accessSetting/index.tsx
@@ -5,10 +5,49 @@ import WhiteLogo from '../welcome screen/WhiteLogo.svg';
 import { Grid, Typography, Box } from '@material-ui/core';
 import Card from '../../components/Layouts/Card';
 import { Avatar, IconButton, Toolbar } from '@material-ui/core';
-
+import { withStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { purple } from '@material-ui/core/colors';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch, { SwitchClassKey, SwitchProps } from '@material-ui/core/Switch';
 export interface AccessibilityProps {}
 
+const PurpleSwitch = withStyles({
+
+    
+    switchBase: {
+      color: purple[300],
+      '&$checked': {
+        color: purple[500],
+      },
+      '&$checked + $track': {
+        backgroundColor: purple[500],
+      },
+    },
+    checked: {},
+    track: {},
+  })(Switch);
+
+const WhiteTypography = withStyles({
+    root: {
+        color: '#FAFAFA',
+        textAlign: 'left',
+    },
+})(Typography);
+
+
 export default function Accessibility() {
+    const [state, setState] = React.useState({
+        checkedA: true,
+        checkedB: true,
+        checkedC: true
+      });
+      
+
+      const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setState({ ...state, [event.target.name]: event.target.checked });
+      };
+
     return (
         <div style={{ background: '#1b1b1b' }} className="bgg">
                 <Toolbar>
@@ -17,9 +56,32 @@ export default function Accessibility() {
                 <div style={{ color: '#fafafa' }}>
                     <Card  background="#202020" title="Accessibility Setting" split={1}>
                         <div style={{ color: 'black' }}>
-                            <Grid container spacing={4} direction="row" alignItems="center" justify="center">
-                                
-                            </Grid>
+                        <FormGroup>
+                            <WhiteTypography>Dark Mode</WhiteTypography>
+                            
+                            <FormControlLabel
+                                control={<PurpleSwitch checked={state.checkedA} onChange={handleChange} name="checkedA" />}
+                                label=""
+                            />
+                            </FormGroup>
+                            <br></br>
+                            <FormGroup>
+                            <WhiteTypography>Change to monochromatic colors</WhiteTypography>
+                            
+                            <FormControlLabel
+                                control={<PurpleSwitch checked={state.checkedB} onChange={handleChange} name="checkedB" />}
+                                label=""
+                            />
+                            </FormGroup>
+                            <br></br>
+                            <FormGroup>
+                            <WhiteTypography>Increase overall text size</WhiteTypography>
+                            
+                            <FormControlLabel
+                                control={<PurpleSwitch checked={state.checkedC} onChange={handleChange} name="checkedC" />}
+                                label=""
+                            />
+                            </FormGroup>
                         </div>
                     </Card>
                 </div>
