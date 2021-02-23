0a468383c471c5c9d316e6f11a816ef72408bfe4
adt6 <aditya.thakur5753235@gmail.com>
2021-02-22 22:46:03 -0800

    Notification
    Please align the button next to the push notification text
    
    Co-Authored-By: Mohit Katta <55575735+mk210@users.noreply.github.com>

--- src/pages/notificationset/index.tsx
+++ src/pages/notificationset/index.tsx
@@ -4,10 +4,48 @@ import WhiteLogo from '../welcome screen/WhiteLogo.svg';
 import { Grid, Typography, Box } from '@material-ui/core';
 import Card from '../../components/Layouts/Card';
 import { Avatar, IconButton, Toolbar } from '@material-ui/core';
-
+import { withStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { purple } from '@material-ui/core/colors';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch, { SwitchClassKey, SwitchProps } from '@material-ui/core/Switch';
 export interface NotificationProps {}
 
+const PurpleSwitch = withStyles({
+
+    
+    switchBase: {
+      color: purple[300],
+      '&$checked': {
+        color: purple[500],
+      },
+      '&$checked + $track': {
+        backgroundColor: purple[500],
+      },
+    },
+    checked: {},
+    track: {},
+  })(Switch);
+
+const WhiteTypography = withStyles({
+    root: {
+        color: '#FAFAFA',
+        textAlign: 'left',
+    },
+})(Typography);
+
 export default function Notification() {
+
+    const [state, setState] = React.useState({
+        checkedA: true
+      });
+      
+
+      const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setState({ ...state, [event.target.name]: event.target.checked });
+      };
+
+
     return (
         <div style={{ background: '#1b1b1b' }} className="bgg">
                 <Toolbar>
@@ -16,9 +54,16 @@ export default function Notification() {
                 <div style={{ color: '#fafafa' }}>
                     <Card  background="#202020" title="Notification Setting" split={1}>
                         <div style={{ color: 'black' }}>
-                            <Grid container spacing={4} direction="row" alignItems="center" justify="center">
-                                
-                            </Grid>
+                            {/* <Grid container spacing={4} direction="row" alignItems="center" justify="center"> */}
+                            <FormGroup>
+                            <WhiteTypography>Push notification </WhiteTypography>
+                            
+                            <FormControlLabel
+                                control={<PurpleSwitch checked={state.checkedA} onChange={handleChange} name="checkedA" />}
+                                label=""
+                            />
+                            </FormGroup>
+                            {/* </Grid> */}
                         </div>
                     </Card>
                 </div>
