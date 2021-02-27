37f9ce8de450be346e298fef1e374a07f93d2797
syedkhajahussainsa <55559578+syedkhajahussainsa@users.noreply.github.com>
2021-02-27 17:08:41 +0400

    camera update

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ import Notification from './pages/notificationset';
 import ViewPoints from './pages/view-points-screen/points';
 import EditProfile from './pages/edit-profile/editProfile';
 import LocationPicker from './components/Inputs/LocationPicker';
+import WebCamFun from './pages/camera/index'
 // import GTLexpanded from './components/Display/GTLexpanded';
 function App(): JSX.Element {
     return (
@@ -74,6 +75,9 @@ function App(): JSX.Element {
                     </Route>
                     <Route exact path="/EditProfile">
                         <EditProfile />
+                    </Route>
+                    <Route exact path="/webcam">
+                        <WebCamFun />
                     </Route>
                     {/* <Route exact path="/GTLexpanded">
                         <GTLexpanded />
@@ -142,6 +146,10 @@ function Nav() {
                     <li>
                         <Link to="/EditProfile">Edit Profile</Link>
                     </li>
+                    <li>
+                        <Link to="/webcam">camera</Link>
+                    </li>
+
                     {/* <li>
                         <Link to="/GTLexpanded">GTL Expanded component</Link>
                     </li> */}
---
+++ src/pages/camera/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './styles.scss';
+import Webcam from "react-webcam";
+export interface WebCamFunProps {}
+export default class WebCamFun extends React.Component<WebCamFunProps> {
+    public render(): JSX.Element {
+        return (
+            <div>
+                <div className="bg">
+                    <Webcam/>         
+                </div>
+            </div>
+        );
+    }
+}
