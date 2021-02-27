bf909ba423a5b13dbf3fce16cc7ed20873480d98
hsggamer <55681300+hsggamer@users.noreply.github.com>
2021-02-27 23:37:24 +0400

    Infinite scroll

---
+++ src/components/Layouts/Posts.tsx
@@ -0,0 +1,58 @@
+import firebase from 'firebase';
+
+export default {
+  /**
+   * this function will be fired when you first time run the app,
+   * and it will fetch first 5 posts, here I retrieve them in descending order, until the last added post appears first.
+   */
+  postsFirstBatch: async function () {
+    try {
+      const data = await firebase
+      .firestore()
+      .collection('Posts')
+      .orderBy('likes_count')
+        .limit(5)
+        .get();
+
+      let posts:Array<any> = [];
+      let lastKey:firebase.firestore.QueryDocumentSnapshot<firebase.firestore.DocumentData> = "" as unknown as firebase.firestore.QueryDocumentSnapshot<firebase.firestore.DocumentData>;
+      data.forEach((doc) => {
+        posts.push(doc.data());
+        lastKey = (doc)
+      });
+      console.log(lastKey)
+      return { posts, lastKey };
+    } catch (e) {
+      console.log(e);
+    }
+  },
+
+  /**
+   * this function will be fired each time the user click on 'More Posts' button,
+   * it receive key of last post in previous batch, then fetch next 5 posts
+   * starting after last fetched post.  
+   */
+  postsNextBatch: async (key:firebase.firestore.QueryDocumentSnapshot<firebase.firestore.DocumentData>) => {
+    try {
+      const data = await firebase
+      .firestore()
+      .collection('Posts')
+      .orderBy('likes_count')
+        .startAfter(key)
+        .limit(5)
+        .get();
+
+      let posts:Array<any> = [];
+      let lastKey:firebase.firestore.QueryDocumentSnapshot<firebase.firestore.DocumentData> = "" as unknown as firebase.firestore.QueryDocumentSnapshot<firebase.firestore.DocumentData>;
+
+      data.forEach((doc) => {
+        posts.push(doc.data());
+        lastKey = (doc);
+      });
+      
+      return { posts, lastKey };
+    } catch (e) {
+      console.log(e);
+    }
+  }
+};
--- src/components/Layouts/feed.tsx
+++ src/components/Layouts/feed.tsx
@@ -2,26 +2,64 @@ import * as React from 'react';
 import { useState, useEffect } from 'react';
 import firebase from 'firebase';
 import SinglePostNew from '../Display/singlePostNew';
+import Post from "./Posts";
 
 export default function Feed() {
     const [posts, setPosts] = useState<any[]>([]);
+    const [lastKey, setLastKey] = useState("" as unknown  as firebase.firestore.QueryDocumentSnapshot<firebase.firestore.DocumentData>);
+    const [nextPosts_loading, setNextPostsLoading] = useState(false);
+
+    // useEffect(() => {
+    //     firebase
+    //         .firestore()
+    //         .collection('Posts')
+    //         .onSnapshot((snapshot: any) => {
+    //             setPosts(snapshot.docs.map((doc: any) => ({ id: doc.id, post: doc.data() })));
+    //         });
+    // }, []);
 
     useEffect(() => {
-        firebase
-            .firestore()
-            .collection('Posts')
-            .onSnapshot((snapshot: any) => {
-                setPosts(snapshot.docs.map((doc: any) => ({ id: doc.id, post: doc.data() })));
-            });
-    }, []);
+        // first 5 posts
+        Post.postsFirstBatch()
+          .then((res) => {
+            if (res) {
+            setPosts(res.posts);
+            setLastKey(res.lastKey);
+          } else return
+        })
+          .catch((err) => {
+            console.log(err);
+          });
+      }, []);
 
-    return (
+      const fetchMorePosts = (key:firebase.firestore.QueryDocumentSnapshot<firebase.firestore.DocumentData>) => {
+          console.log("hello thereee")
+        if (key ) {
+          setNextPostsLoading(true);
+          Post.postsNextBatch(key)
+            .then((res) => {
+              if (res) {
+              setLastKey(res.lastKey);
+              // add new posts to old posts
+              setPosts(posts.concat(res.posts));
+              setNextPostsLoading(false);
+            } else return
+          })
+            .catch((err) => {
+              console.log(err);
+              setNextPostsLoading(false);
+            });
+        }
+      };
+    
+    const allPosts = (
         <div>
-            {posts.map(({ id, post }) => {
-                return (
+            {posts.map((post) => {
+                if (post) {
+                  return (
                     <SinglePostNew
-                        key={id}
-                        id={id}
+                        key={post.id}
+                        id={post.id}
                         // profileUrl={post.profileUrl}
                         username={post.user_name}
                         postPic={post.Image}
@@ -36,7 +74,45 @@ export default function Feed() {
                         comments_count={post.comments_count}
                     />
                 );
+                } else return;
             })}
+        </div>
+    );
+    console.log(lastKey)
+    return (
+        // <div>
+        //     {posts.map(({ id, post }) => {
+        //         return (
+        //             <SinglePostNew
+        //                 key={id}
+        //                 id={id}
+        //                 // profileUrl={post.profileUrl}
+        //                 username={post.user_name}
+        //                 postPic={post.Image}
+        //                 uid={post.uid}
+        //                 // caption={post.caption}
+        //                 // comments={post.comments}
+        //                 date={new Date(post.post_time.seconds * 1000).toLocaleDateString('en-US')}
+        //                 likes_count={post.likes_count}
+        //                 caption={post.caption}
+        //                 sharedURL={window.location.href}
+        //                 hidden={false}
+        //                 comments_count={post.comments_count}
+        //             />
+        //         );
+        //     })}
+        // </div>
+        <div>
+            <div>{posts ? allPosts : null}</div>
+            <div style={{ textAlign: "center" }}>
+        {nextPosts_loading ? (
+          <p>Loading..</p>
+        ) : ((lastKey) ? (
+          <button onClick={() => fetchMorePosts(lastKey)}>More Posts</button>
+        ) : (
+          <span>You are up to date!</span>
+        ))}
+      </div>
         </div>
     );
 }
