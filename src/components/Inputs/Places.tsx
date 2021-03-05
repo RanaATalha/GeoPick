0d2a3dedda92bb99c3c918c0dc9492b83876340a
JeetSolanki999 <jeetsolanki3012@gmail.com>
2021-03-05 21:20:11 +0300

    API key update
    api key works, need to fix key attr.

--- src/components/Inputs/Places.tsx
+++ src/components/Inputs/Places.tsx
@@ -4,25 +4,13 @@ import TextField from './TextField';
 
 export default function Places(props: any) {
     const [address, setAddress] = React.useState('');
-    const [coordinates, setCoordinates] = React.useState({
-        lat: null,
-        lng: null
-    });
-
-    const handleSelect = async (value: any) => {
-        const results = await geocodeByAddress(value);
-        const latLng = await getLatLng(results[0]);
-        setAddress(value);
-        props.address(address);
-        setCoordinates((latLng as unknown) as { lat: null; lng: null });
-    };
 
     return (
         <div>
-            <PlacesAutocomplete value={address} onChange={setAddress} onSelect={handleSelect}>
+            <PlacesAutocomplete value={address} onChange={setAddress}>
                 {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
                     <div>
-                        <TextField {...getInputProps({ placeholder: 'Add Location' })}/>
+                        <TextField {...getInputProps({ placeholder: 'Add Location' })} />
 
                         <div>
                             {loading ? <div>...loading</div> : null}
