67dc7ea01fb10884d5a9ff2e6448fa638b40f291
ms374-HW <ms374@hw.ac.uk>
2021-03-15 17:59:35 +0400

    explore locations

--- src/pages/explore-page/index.tsx
+++ src/pages/explore-page/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useLayoutEffect, useRef, useEffect } from 'react'
+import React, { useState, useLayoutEffect, useRef, useCallback } from 'react'
 import WhiteLogo from '../welcome screen/WhiteLogo.svg';
 import Card from '../../components/Layouts/Card';
 import { RegularBtn } from '../../components/Buttons/RegularBtn';
@@ -9,11 +9,18 @@ import firebase from 'firebase';
 import ProfileOverview from '../../components/Display/profileOverview';
 import Button from '@material-ui/core/Button';
 import SinglePostNew from '../../components/Display/singlePostNew';
+import Places from '../../components/Inputs/Places';
+
 import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import { Typography } from '@material-ui/core';
 import Avatar from '@material-ui/core/Avatar';
 import { Link } from 'react-router-dom';
-
+// import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
+import "mapbox-gl/dist/mapbox-gl.css";
+import Geocoder from 'react-map-gl-geocoder';
+// import Geocoder from "@mapbox/mapbox-gl-geocoder";
+// import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 
 
 export interface SearchProps {}
@@ -21,6 +28,7 @@ export interface SearchProps {}
 export default function ExploreScreen() {
     const [posts, setPosts] = useState<any[]>([]);
     const firstUpdate = useRef(true);
+    var mapRef = useRef();
     const [viewport, setViewport] = useState({
         latitude: 45.4211,
         longitude: -75.6903,
@@ -34,16 +42,35 @@ export default function ExploreScreen() {
           firstUpdate.current = false;
           return;
         }
-    
-        // console.log("componentDidUpdateFunction");
-        firebase
+
+          firebase
             .firestore()
             .collection('Posts')
             .onSnapshot((snapshot: any) => {
                 setPosts(snapshot.docs.map((doc: any) => ({ id: doc.id, post: doc.data() })));
             });
+    });
+    
+    const handleViewportChange = useCallback(
+        (newViewport) => setViewport(newViewport),
+        []
+    );
+    
+    // from official documentation
+    const handleGeocoderViewportChange = useCallback(
+        (newViewport) => {
+          const geocoderDefaultOverrides = { transitionDuration: 1000 };
+    
+          return handleViewportChange({
+            ...newViewport,
+            ...geocoderDefaultOverrides
+          });
+        },
+        [handleViewportChange]
+      );
+        // console.log("componentDidUpdateFunction");
+        
         // console.log(posts)
-      });
 
     // useEffect(() => {
         
@@ -59,12 +86,37 @@ export default function ExploreScreen() {
             <div id="titleDiv">
                 <Card background="#202020" title="Explore" split={2}>
                 <ReactMapGL
+                    ref={mapRef}
+                    {...viewport}
+                    width="100%"
+                    onViewportChange={handleViewportChange}
+                    mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+                >
+                    <Geocoder
+                        mapRef={mapRef}
+                        onViewportChange={handleGeocoderViewportChange}
+                        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+                        position="top-left"
+                    />
+                      {posts.map(({id, post}) => (
+                        <Marker
+                            key={id}
+                            latitude={post.coordinates.lat}
+                            longitude={post.coordinates.lng}
+                        >
+                            <Link to={{ pathname: `/post/${id}`, state: post.uid}}>
+                                <Avatar alt={post.user_name} src={post.Image} />
+                            </Link>
+                            
+                        </Marker>
+                    ))}
+                </ReactMapGL>
+                {/* <ReactMapGL
+                    // ref={mapRef}
                     {...viewport}
                     mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-                    mapStyle="mapbox://styles/geopick2021/cklr56ufp069x18pss5neglc4"
-                    onViewportChange={(viewport: any) => {
-                    setViewport(viewport);
-                    }}
+                    mapStyle="mapbox://styles/geopick2021/ckmaljmwxfoku17qodcrj8oa6"
+                    onViewportChange={(viewport: any) => { setViewport(viewport); }}
                 >
                     {posts.map(({id, post}) => (
                         <Marker
@@ -78,8 +130,14 @@ export default function ExploreScreen() {
                             
                         </Marker>
                     ))}
+                    {/* <Geocoder
+                        mapRef={mapRef}
+                        onViewportChange={(viewport: any) => { setViewport(viewport);}
+                        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+                        position="top-left"
+                    /> */}
 
-                </ReactMapGL>
+                {/* </ReactMapGL> */}
                  
                   
                 </Card>
